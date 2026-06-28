fa19fda3786f3291e525df416435adbc62cd1842
Freynaldii <s160423059@student.ubaya.ac.id>
2026-06-28 20:15:56 +0700

    Hapus komentar

--- projectVitaGuard/resources/views/categories/getEditFormB.blade.php
+++ projectVitaGuard/resources/views/categories/getEditFormB.blade.php
@@ -1,9 +1,3 @@
-{{--
-    categories/getEditFormB.blade.php
-    Digunakan untuk Edit Type B: update data via Ajax TANPA reload halaman.
-    Tidak pakai <form> tag - submit ditangani oleh fungsi saveDataUpdate() di JS.
---}}
-
 <h5 class="mb-3">Update Category</h5>
 
 <div class="mb-3">
