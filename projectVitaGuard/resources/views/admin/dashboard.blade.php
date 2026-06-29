5f068e82047a2d6c2521d5c28c21efdd7ef6d5b2
Freynaldii <s160423059@student.ubaya.ac.id>
2026-06-30 01:38:03 +0700

    Update dashboard.blade.php

--- projectVitaGuard/resources/views/admin/dashboard.blade.php
+++ projectVitaGuard/resources/views/admin/dashboard.blade.php
@@ -95,7 +95,7 @@
     </div>
 @endsection
 
-@push('scripts')
+@push('script')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         const ctx = document.getElementById('statsChart');
